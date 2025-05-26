73c55ced5b2d7a8b509ca0c1860b783570db9a6e
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-05-27 01:11:53 +0530

    hero-section done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingCart, FaSearch, FaBars } from "react-icons/fa";
 import { FiArrowRight } from "react-icons/fi";
+import { ArrowRight } from "lucide-react";
 
 
 const Header = () => {
   return (
     <div className="bg-white shadow px-6 py-4 flex justify-between items-center">
       <Link to="/" className="flex flex-col leading-tight">
-        <span className="text-2xl font-bold text-blue-600">OmniVus.</span>
+        <span className="text-3xl font-bold text-blue-700">OmniVus.</span>
         <span className="text-sm text-gray-500">IT Services & Technology</span>
       </Link>
       <div className="flex space-x-6 justify-center items-center text-lg">
@@ -32,7 +33,7 @@ const Header = () => {
         </Link>
         <button className="flex justify-center items-center bg-blue-700 text-sm text-white font-serif font-semibold p-4 rounded hover:bg-white hover:text-blue-700 border border-blue-700 transition duration-300 ">
           Free Consulting
-          <FiArrowRight className="text-white-500 opacity-75 text-xl" />
+          <ArrowRight className="text-white-500 opacity-75 text-xl" />
         </button>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { ArrowRight } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div></div>
-  )
-}
+    // Hero Section :
+    <main className="min-h-screen bg-white font-sans">
+      {/* Fullscreen Background Image */}
+      <div className="h-screen bg-[url('https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg?cs=srgb&dl=pexels-francesco-ungaro-281260.jpg&fm=jpg')] bg-cover bg-center">
+        {/* Optional Dark Overlay */}
+        <div className="h-full w-full bg-black bg-opacity-50 flex items-center">
+          <section className="w-full">
+            <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row items-center justify-between w-full">
+              {/* Left Text */}
+              <div className="md:w-3/5 space-y-6 text-white">
+                <span className="text-lg uppercase tracking-widest text-blue-300 font-semibold">
+                  IT Business Consulting
+                </span>
+                <h1 className="text-2xl md:text-6xl font-extrabold leading-tight font-serif tracking-tight">
+                  Best IT Solutions <br /> Provider Agency
+                </h1>
+                <p className="text-gray-200 text-lg max-w-xl font-light leading-relaxed">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque totam rem aperiam.
+                </p>
+                <Link to="/services">
+                  <button className="mt-4 inline-flex items-center px-6 py-3 bg-blue-800 text-white rounded hover:bg-white hover:text-blue-700 transition duration-300 font-medium">
+                    Our Services <ArrowRight className="ml-2 h-4 w-4" />
+                  </button>
+                </Link>
+              </div>
 
-export default Home
+              {/* Right Image */}
+              <div className="md:w-3/5 mt-10 md:mt-0 flex justify-end pr-18">
+                <img
+                  src="https://html.webtend.net/omnivus/assets/img/banner/banner-img.png"
+                  alt="Work"
+                  className="w-[100%] max-w-md md:max-w-lg"
+                />
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+      {/* What-We-Do */}
+    </main>
+  );
+};
+
+export default Home;
