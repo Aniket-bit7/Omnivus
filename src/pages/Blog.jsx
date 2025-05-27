8ea1342040ae9b828621fe90b97a0b03494ff82f
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-05-27 18:58:52 +0530

    working on both services and blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Navbar from "../components/Navbar"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <main className="min-h-screen bg-white font-sans">
+      {/* Navbar */}
+      <Navbar/>
+      <section className="flex flex-col justify-center items-center mt-16">
+        <h1 className="font-extrabold text-7xl text-blue-950">News Standard</h1>
+        <h3 className="font-bold text-sky-600 text-2xl">Home | News</h3>
+      </section>
+    </main>
   )
 }
 
